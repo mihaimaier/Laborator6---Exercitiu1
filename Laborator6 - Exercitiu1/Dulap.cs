33787a770864e2589514994cf2d72fcb9b79714b
mihaimaier <maiermihai90@gmail.com>
2022-06-16 01:00:13 +0300

    Add project files.

---
+++ Laborator6 - Exercitiu1/Dulap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laborator6___Exercitiu1
+{
+    class Dulap
+    {
+        private int lungime;
+        private int latime;
+        private int inaltime;
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+        public int GetVolumulDulapului()
+        {
+            return lungime * latime * inaltime;
+        }
+        public void TiparesteVolumul()
+        {
+            Console.WriteLine($"Dulapul are o lungime de {lungime}, o latime de {latime} si o inaltime de {inaltime}. Volumul dulapului este de: {GetVolumulDulapului()} ");
+        }
+    }
+}
---
+++ Laborator6 - Exercitiu1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Laborator6___Exercitiu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Laborator 6 - Exercitii
+
+            //Exercitiu 1
+
+            //Un dulap are trei caracteristici: lungime, latime, inaltime.
+            //Dulapul va avea o metoda care va calcula volumul acestuia. 
+            //Folositi o clasa care va modela un dulap. 
+            //Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+            //Cele trei campuri vor fi initializate cu ajutorul constructorului
+
+            // - Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri
+            // - Realiazti diagrama UML a clasei.
+            // - Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si afisati rezultatul
+
+            Dulap dulap = new Dulap(3, 10, 1);
+            dulap.TiparesteVolumul();
+        }
+    }
+}
